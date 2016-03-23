f705ff432fb3f2bbf6f78a7a66748188c298a425
Scott Riley <scott.riley111@gmail.com>
2016-03-23 11:30:24 +0000

    adds a .env file and references it in the sql connection

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,13 +1,13 @@
+require 'pg'
+require 'dotenv' if ENV["DATABASE_URL"].nil?
+Dotenv.load if ENV["DATABASE_URL"].nil?
 class SqlRunner
 
   def self.run_sql(query)
 
     begin
-
-      db = PG.connect(dbname: "app_db", host: "localhost") if development?
-      db = PG.connect( { host: "ec2-54-228-246-42.eu-west-1.compute.amazonaws.com", user: "yxlddfjywgxdbr", dbname: "d48gou3to3j84a", password: "f3BmTeaM5icv9ju5ZytdeDljHu"} ) if production?
+      db = PG.connect(ENV["DATABASE_URL"])
       result = db.exec(query)
-
     ensure
       db.close
     end
